a29b97d959f9a88b266483c480e8b8d42a77989c
Cory <nelson.cory@outlook.com>
2024-01-07 23:17:55 -0600

    Initialize Event System

---
+++ js/components/eventBox.js
@@ -0,0 +1,13 @@
+class EventBox extends Phaser.GameObjects.Container{
+    constructor(config){
+        this.scene = config.scene;
+
+        //eventID
+        //EventName
+        //eventPicture
+        //content (blurb)
+        //choices - array of buttons
+    }
+
+
+}
